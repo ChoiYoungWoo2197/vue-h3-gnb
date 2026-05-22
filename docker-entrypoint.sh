d905eb2c12055ade2eae884dc1835517246b4382
ChoiYoungWoo2197 <duddn2197@naver.com>
2026-05-22 09:25:02 +0900

    Refactor: DB 환경변수 분리, MariaDB 준비 대기 루프 적용

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 set -e
 
+DB_NAME=${DB_NAME:-gnuboard}
+DB_USER=${DB_USER:-gnuboard}
+DB_PASS=${DB_PASS:-gnuboard123}
+
 # MariaDB 초기화 (최초 1회)
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
     mysqld_safe --skip-networking &
-    sleep 5
+
+    # MariaDB 준비 완료까지 대기 (최대 30초)
+    for i in $(seq 1 30); do
+        if mysqladmin ping --silent 2>/dev/null; then
+            break
+        fi
+        sleep 1
+    done
 
     mysql -u root <<-EOSQL
-        CREATE DATABASE IF NOT EXISTS gnuboard CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-        CREATE USER IF NOT EXISTS 'gnuboard'@'localhost' IDENTIFIED BY 'gnuboard123';
-        GRANT ALL PRIVILEGES ON gnuboard.* TO 'gnuboard'@'localhost';
+        CREATE DATABASE IF NOT EXISTS ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+        CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';
+        GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'localhost';
         FLUSH PRIVILEGES;
 EOSQL
 
